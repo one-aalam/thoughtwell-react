ea60dddb6263237b9545d787306dcded18d0806c
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2022-02-15 17:29:57 +0530

    01.a - Just something to render

---
+++ 01-get-started/main.js
@@ -0,0 +1,5 @@
+document.querySelector('#app').innerHTML = `
+    <img width="60" src="./favicon.svg" alt="logo" />
+    <h1>thought<u style="color: indigo">well</u></h1>
+    <p>where people connnect on what they're up to...</p>
+`
