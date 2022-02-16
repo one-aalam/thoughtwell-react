1b8f121f4cd8b0e24efec741be714d6ed5d38ba7
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2022-02-16 09:31:48 +0530

    01.e - structuring for re-use

--- 01-get-started/src/main.js
+++ 01-get-started/src/main.js
@@ -1,16 +1,22 @@
 import logoSvg from './favicon.svg'
 import './styles.css'
 
-const App = () => {
-    return React.createElement('div', {},
+const Header = () => {
+    return React.createElement('div', { className: 'header' },
         React.createElement('img', { src: logoSvg , alt: 'logo', width: 60 }),
         React.createElement('h1', {},
             React.createElement('span', {}, 'thought'),
             React.createElement('u', { style: { color: 'indigo'}}, 'well')
         ),
-        React.createElement('p', {}, 'where people connnect on what they\'re up to...')
+        React.createElement('h6', {}, 'where people connnect on what they\'re up to...'),
     )
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById('app')
-);
+const App = () => {
+    return React.createElement('div',
+        { className: 'container' },
+        React.createElement(Header),
+    )
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById('app'));
