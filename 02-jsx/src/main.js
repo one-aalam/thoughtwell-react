ab4b3901d9fb058cdc14cc1afb796280e02b0e0d
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2022-02-16 10:06:56 +0530

    02.a - elements of re-usability - part-1

--- 02-jsx/src/main.js
+++ 02-jsx/src/main.js
@@ -1,6 +1,38 @@
 import logoSvg from './favicon.svg'
 import './styles.css'
 
+function User() {
+    return React.createElement("div", {
+      className: "user-cell"
+    },
+        React.createElement("img", {
+            className: "user-cell__avatar",
+            src: "https://avatars.dicebear.com/api/open-peeps/johndoe.svg",
+            alt: "John Doe"
+        }),
+        React.createElement("div", {
+            className: "user-cell__info"
+        },
+            React.createElement("h3", null, "John Doe"),
+            React.createElement("h6", null, "@" + "johndoe")
+        ),
+        React.createElement("div", {
+            className: "user-cell__actions"
+        },
+            React.createElement("button", null, "follow")
+        )
+    );
+}
+
+function UserList() {
+    return React.createElement("div", {
+        className: "user-list"
+      },
+      React.createElement("h4", { className: "user-list__title"}, 'Who to follow'),
+      React.createElement(User, {}),
+    )
+}
+
 const Header = () => {
     return React.createElement('div', { className: 'header' },
         React.createElement('img', { src: logoSvg , alt: 'logo', width: 60 }),
@@ -16,6 +48,7 @@ const App = () => {
     return React.createElement('div',
         { className: 'container' },
         React.createElement(Header),
+        React.createElement(UserList),
     )
 }
 
