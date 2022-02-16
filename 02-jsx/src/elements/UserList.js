197d4279ae4c981ab75fff552498d31aa336ccf9
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2022-02-16 11:15:16 +0530

    02.e - Keep data and components separate

--- 02-jsx/src/elements/UserList.js
+++ 02-jsx/src/elements/UserList.js
@@ -1,14 +1,16 @@
 import User from './User'
 
-export default function UserList() {
+export default function UserList({ users }) {
     return React.createElement("div", {
         className: "user-list"
       },
       React.createElement("h4", { className: "user-list__title"}, 'Who to follow'),
-      [
-        React.createElement(User, { key: 'amar', name: 'Amar', handle: 'amar'} ),
-        React.createElement(User, { key: 'akbar', name: 'Akbar', handle: 'akbar'}),
-        React.createElement(User, { key: 'anthony', name: 'Anthony', handle: 'anthony'}),
-      ]
+      users.map(
+            user => React.createElement(User, {
+                key: user.handle,
+                name: user.name ,
+                handle: user.handle
+            })
+      )
     )
 }
--- 02-jsx/src/main.js
+++ 02-jsx/src/main.js
@@ -3,11 +3,17 @@ import './styles.css'
 import Header from './elements/Header'
 import UserList from './elements/UserList'
 
+const USERS = [
+    { name: 'Amar', handle: 'amar'},
+    { name: 'Akbar', handle: 'akbar'},
+    { name: 'Anthony', handle: 'anthony'}
+]
+
 const App = () => {
     return React.createElement('div',
         { className: 'container' },
         React.createElement(Header),
-        React.createElement(UserList),
+        React.createElement(UserList, { users: USERS }),
     )
 }
 
