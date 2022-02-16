70032a4286d6993ed28022231e0e6c18fca95961
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2022-02-16 18:06:11 +0530

    03.h - JSX - use `Fragment`

--- 03-jsx/src/App.jsx
+++ 03-jsx/src/App.jsx
@@ -7,10 +7,17 @@ const USERS = [
     { name: 'Anthony', handle: 'anthony', isFollowed: false }
 ]
 
+const UserListTitle = () =>
+    <React.Fragment>
+        Wanna be <strong>friends</strong> with?
+        <br/>
+        <small style={{fontSize: '0.8rem', textTransform: 'lowercase'}}><em>thoughtwell reccommends the following folks...</em></small>
+    </React.Fragment>
+
 const App = () => (
     <div className="container">
         <Header/>
-        <UserList users={USERS}>Wanna be <strong>friends</strong> with?</UserList>
+        <UserList users={USERS}><UserListTitle/></UserList>
     </div>
 )
 
