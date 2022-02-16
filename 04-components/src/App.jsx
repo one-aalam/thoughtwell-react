8b4677625d36391fec83fd546711c7ed3edee3d9
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2022-02-16 18:42:44 +0530

    04.a - `element` -> `component`

--- 04-components/src/App.jsx
+++ 04-components/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './elements/Header'
-import UserList from './elements/UserList'
+import Header from './components/Header'
+import UserList from './components/UserList'
 
 const USERS = [
     { name: 'Amar', handle: 'amar', isFollowed: false },
