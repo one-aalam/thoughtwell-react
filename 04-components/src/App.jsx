09210d6d8ec7f6822b7143590644b9c9378e7160
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2022-02-16 21:14:55 +0530

    04.e - Stats and stateful-ness

--- 04-components/src/App.jsx
+++ 04-components/src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
 import UserList from './components/UserList'
 
 const USERS = [
     { name: 'Amar', handle: 'amar', isFollowed: false },
-    { name: 'Akbar', handle: 'akbar', isFollowed: true },
+    { name: 'Akbar', handle: 'akbar', isFollowed: false },
     { name: 'Anthony', handle: 'anthony', isFollowed: false }
 ]
 
@@ -15,11 +15,27 @@ const UserListTitle = () =>
         <small style={{fontSize: '0.8rem', textTransform: 'lowercase'}}><em>thoughtwell reccommends the following folks...</em></small>
     </React.Fragment>
 
-const App = () => (
-    <div className="container">
-        <Header/>
-        <UserList users={USERS} onAction={(isFollowed, user) => console.log(isFollowed, user)}><UserListTitle/></UserList>
-    </div>
-)
+const App = () => {
+    const [ followedUsers, setFollowedUsers ] = useState([])
+
+    return (
+        <div className="container">
+            <Header/>
+            <UserList users={USERS} onAction={(isFollowed, user ) => {
+                setFollowedUsers(users => {
+                    if(isFollowed) {
+                        return [ ...users, user ]
+                    } else {
+                        return users.filter(_user => _user.handle !== user.handle )
+                    }
+                })
+            }}>
+                <strong className={`follow-stats ${followedUsers.length ? 'follow-stats--followed': ''}`}>Following: { followedUsers.length ? `${followedUsers.length} person(s)` : 'nobody' }</strong>
+                <br/>
+                <UserListTitle/>
+            </UserList>
+        </div>
+    )
+}
 
 export default App
