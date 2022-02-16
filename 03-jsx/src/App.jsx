a38808e4d5553791b88f302a95d3e0416c0efe92
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2022-02-16 16:02:16 +0530

    03.b - JSX - play with properties - part 1

--- 03-jsx/src/App.jsx
+++ 03-jsx/src/App.jsx
@@ -2,9 +2,9 @@ import Header from './elements/Header'
 import UserList from './elements/UserList'
 
 const USERS = [
-    { name: 'Amar', handle: 'amar'},
-    { name: 'Akbar', handle: 'akbar'},
-    { name: 'Anthony', handle: 'anthony'}
+    { name: 'Amar', handle: 'amar', isFollowed: false },
+    { name: 'Akbar', handle: 'akbar', isFollowed: true },
+    { name: 'Anthony', handle: 'anthony', isFollowed: false }
 ]
 
 const App = () => (
--- 03-jsx/src/elements/User.jsx
+++ 03-jsx/src/elements/User.jsx
@@ -1,4 +1,12 @@
-export default function User({ name, handle }) {
+const FollowButton = ({ name, isFollowed = false }) => {
+    return (
+        <button className="follow-btn" disabled={isFollowed} onClick={() => alert(`Mr. ${name} is followed now!`) }>
+            follow
+        </button>
+    )
+}
+
+export default function User({ name, handle, isFollowed }) {
     return (
         <div className="user-cell">
             <img className="user-cell__avatar" src={`https://avatars.dicebear.com/api/open-peeps/${handle}.svg`} alt={name} />
@@ -7,7 +15,7 @@ export default function User({ name, handle }) {
                 <h6>{`@${handle}`}</h6>
             </div>
             <div className="user-cell__actions">
-                <button onClick={() => alert(`Mr. ${name} is followed now!`) }>follow</button>
+                <FollowButton name={name} isFollowed={isFollowed} />
             </div>
         </div>
     )
