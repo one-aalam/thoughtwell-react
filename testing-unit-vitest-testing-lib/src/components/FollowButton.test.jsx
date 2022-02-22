26bda1c39d778ef754c86139aedb1ec46e96c932
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2022-02-22 21:05:25 +0530

    unit testing ft. vitest and Testing Lib - Component interaction testing

--- testing-unit-vitest-testing-lib/src/components/FollowButton.test.jsx
+++ testing-unit-vitest-testing-lib/src/components/FollowButton.test.jsx
@@ -1,13 +1,49 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
+import { vi } from 'vitest'
 import FollowButton from './FollowButton'
 import { USERS } from '../mocks/fixtures/users'
 
 describe('FollowButton', () => {
-    test('renders FollowButton with correct states', () => {
+    test('renders FollowButton with correct states/labels', () => {
         render(<FollowButton user={USERS[0]} />)
 
-        expect(screen.getByRole('button')).toBeInTheDocument()
-        expect(screen.getByRole('button')).toHaveTextContent('follow')
+        const followBtn = screen.getByRole('button')
+        expect(followBtn).toBeInTheDocument()
+        // expect(followBtn).toHaveTextContent('follow') // not this
+        expect(followBtn.innerHTML).toEqual('follow') // but, this!
+
+        fireEvent.click(followBtn)
+        expect(followBtn.innerHTML).toEqual('unfollow')
+
+        fireEvent.click(followBtn)
+        expect(followBtn.innerHTML).toEqual('follow')
+    })
+
+    test('emits correct payload to onClick listener', () => {
+        const onClick = vi.fn()
+        const { name, handle, isFollowed } = USERS[0]
+        render(
+            <FollowButton
+                user={{ name, handle, isFollowed }}
+                onClick={onClick}
+            />
+        )
+
+        const followBtn = screen.getByRole('button')
+
+        fireEvent.click(followBtn)
+        expect(onClick).toHaveBeenCalledTimes(1)
+        expect(onClick).toHaveBeenCalledWith(true, {
+            name,
+            handle,
+        })
+
+        fireEvent.click(followBtn)
+        expect(onClick).toHaveBeenCalledTimes(2)
+        expect(onClick).toHaveBeenCalledWith(false, {
+            name,
+            handle,
+        })
     })
 })
---
+++ testing-unit-vitest-testing-lib/src/components/UserSearchForm.test.jsx
@@ -0,0 +1,49 @@
+import '@testing-library/jest-dom'
+import { render, screen, fireEvent, cleanup } from '@testing-library/react'
+import { vi } from 'vitest'
+import UserSearchForm from './UserSearchForm'
+
+afterEach(cleanup)
+
+describe('UserSearchForm', () => {
+    test('renders UserSearchForm component (with focus)', () => {
+        render(<UserSearchForm />)
+
+        expect(screen.getByRole('form')).toBeInTheDocument()
+        expect(screen.getByLabelText('Search')).toBeInTheDocument()
+        expect(screen.getByLabelText('Search').value).toBe('')
+        expect(screen.getByLabelText('Search')).toHaveFocus()
+    })
+
+    test('reflects the values provided by the user', () => {
+        render(<UserSearchForm />)
+
+        fireEvent.change(screen.getByLabelText('Search'), {
+            target: { value: 'typed in this...' },
+        })
+        expect(screen.getByLabelText('Search').value).toEqual(
+            'typed in this...'
+        )
+    })
+
+    test('submits with empty value w/o any user provided input', () => {
+        const onSearch = vi.fn()
+        render(<UserSearchForm onSearch={onSearch} />)
+
+        fireEvent.submit(screen.getByRole('form'))
+        expect(onSearch).toHaveBeenCalled()
+        expect(onSearch).toHaveBeenCalledWith('')
+    })
+
+    test('submits with provided value when user provides input', () => {
+        const onSearch = vi.fn()
+        render(<UserSearchForm onSearch={onSearch} />)
+
+        fireEvent.change(screen.getByLabelText('Search'), {
+            target: { value: 'typed in this...' },
+        })
+        fireEvent.submit(screen.getByRole('form'))
+        expect(onSearch).toHaveBeenCalled()
+        expect(onSearch).toHaveBeenCalledWith('typed in this...')
+    })
+})
